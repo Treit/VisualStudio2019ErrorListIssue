72da9262b88d516318c05e618bef1f65b4de13c9
Mike Treit <mtreit@ntdev.microsoft.com>
2019-04-26 13:11:22 -0700

    Fix up the screenshot to reflect the demonstration code.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello World!");
         }
     }
 }
